31c4c09cbd6d7872575ded79ac800871b05dc35e
Beatacodetop <beata9sidor@gmail.com>
2025-05-12 09:30:46 +0200

    wip: gui

--- notatnik.py
+++ notatnik.py
@@ -1,5 +1,5 @@
 users = [
-    {"name": "Beata", "location": "Lublin", "posts": 555},
+    {"name": "Beata", "location": "Lublin", "  posts": 555},
     {"name": "Mikołaj", "location": "Przasnysz", "posts": 200},
     {"name": "Krzysztof", "location": "Poznań", "posts": 100},
     {"name": "Bartosz", "location": "Ostrołęka", "posts": 300},
